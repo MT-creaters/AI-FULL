1ba6755f44d38a69735368a1fc53a7b157b3b4c4
MT-creaters <243427037@ccmailg.meijo-u.ac.jp>
2024-04-27 11:28:15 +0900

    ウィンドウ

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ function change_screen(){
 		isPortrait = window.matchMedia("(orientation: portrait)").matches;
 		console.log(isPortrait);
 		// web_cam.value_change();
-		resizeCanvas(windowHeight,windowWidth,);
+		// resizeCanvas(windowHeight,windowWidth,);
 	}
 }
 
