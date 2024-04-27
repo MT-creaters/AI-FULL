f6b8183df396f6129f68a3a43deb307d70a4b026
taniguchikenta <k.k.taniken1011@gmail.com>
2024-04-27 17:59:30 +0900

    check27

--- js/main.js
+++ js/main.js
@@ -258,14 +258,14 @@ class Web_cam{
 		pop();
 	}
 	web_cam_draw_horizon(load ,img){
-		translate(width, 0);
+		translate(width-(width/2), 0);
 		scale(-1, 1);
 		if(this.alpha > 0) load.hidden = true;
 		if (this.alpha <255) this.alpha += 30;
 		tint(255 , this.alpha);
 		// image(img, 0,0, this.scaled[1]/3, this.scaled[0]/3);
 		image(img, 0, 0, (this.scaled[1]*windowHeight)/this.scaled[0], windowHeight)
-		translate(windowWidth/2, 0)
+
 		document.getElementById('yoko').innerHTML="よこ iamge= "+image;
 		pop();
 	}
